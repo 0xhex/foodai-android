ec4fac3f044937355720d9c83d75392f17e8458b
mehmettas <m_tas@outlook.com>
2024-12-05 21:47:06 +0300

    Add Application Class, theming, network configs

--- app/src/main/java/com/codepad/foodai/FoodAIApplication.kt
+++ app/src/main/java/com/codepad/foodai/FoodAIApplication.kt
@@ -1,4 +1,25 @@
 package com.codepad.foodai
 
-class FoodAIApplication {
+import android.app.Application
+import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
+
+@HiltAndroidApp
+class FoodAIApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            plant(Timber.DebugTree())
+        } else {
+            plant(ReleaseTree())
+        }
+    }
+}
+
+internal class ReleaseTree : Timber.Tree() {
+    protected override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        Log.println(priority, tag, message)
+    }
 }
