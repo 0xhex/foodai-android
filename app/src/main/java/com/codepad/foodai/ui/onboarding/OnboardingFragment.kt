983e5b860a06151760461d184ca58f94e5e1824d
mehmettas <m_tas@outlook.com>
2024-12-07 17:18:08 +0300

    Apply page change logics for on boarding

--- app/src/main/java/com/codepad/foodai/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/codepad/foodai/ui/onboarding/OnboardingFragment.kt
@@ -1,6 +1,9 @@
 package com.codepad.foodai.ui.onboarding
 
+import android.os.Bundle
+import android.view.View
 import androidx.viewpager2.widget.ViewPager2
+import com.airbnb.lottie.LottieAnimationView
 import com.codepad.foodai.R
 import com.codepad.foodai.databinding.OnboardingFragmentBinding
 import com.codepad.foodai.ui.core.BaseFragment
@@ -12,15 +15,39 @@ import dagger.hilt.android.AndroidEntryPoint
 class OnboardingFragment : BaseFragment<OnboardingFragmentBinding>() {
     private lateinit var viewPager: ViewPager2
     private lateinit var nextButton: MaterialButton
+    private lateinit var lottieAnimation: LottieAnimationView
 
     override fun getLayoutId() = R.layout.onboarding_fragment
 
     override fun onReadyView() {
         viewPager = binding.viewPager
         nextButton = binding.nextButton
+        lottieAnimation = binding.lottieAnimation
 
         val adapter = OnboardingPagerAdapter(requireActivity())
         viewPager.adapter = adapter
+        binding.dotsIndicator.attachTo(viewPager)
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when (position) {
+                    0 -> {
+                        lottieAnimation.setAnimation(R.raw.scan)
+                        nextButton.setText(R.string.next)
+                    }
+                    1 -> {
+                        lottieAnimation.setAnimation(R.raw.food_calorie)
+                        nextButton.setText(R.string.next)
+                    }
+                    2 -> {
+                        lottieAnimation.setAnimation(R.raw.slim_figure)
+                        nextButton.setText(R.string.get_started)
+                    }
+                }
+                lottieAnimation.playAnimation()
+            }
+        })
 
         nextButton.setOnClickListener {
             val nextItem = viewPager.currentItem + 1
