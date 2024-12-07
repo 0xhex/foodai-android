51a6da5ff9f84fdc90343a0e6f2566adc7c89095
mehmettas <m_tas@outlook.com>
2024-12-07 16:35:48 +0300

    Add fonts, onboarding base

--- app/src/main/java/com/codepad/foodai/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/codepad/foodai/ui/onboarding/OnboardingFragment.kt
@@ -1,19 +1,34 @@
 package com.codepad.foodai.ui.onboarding
 
-import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.codepad.foodai.R
 import com.codepad.foodai.databinding.OnboardingFragmentBinding
 import com.codepad.foodai.ui.core.BaseFragment
+import com.codepad.foodai.ui.onboarding.pager.OnboardingPagerAdapter
+import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnboardingFragment : BaseFragment<OnboardingFragmentBinding>() {
-    private val viewModel: OnboardingViewModel by viewModels()
+    private lateinit var viewPager: ViewPager2
+    private lateinit var nextButton: MaterialButton
 
-    override fun getLayoutId(): Int {
-        return R.layout.onboarding_fragment
-    }
+    override fun getLayoutId() = R.layout.onboarding_fragment
 
     override fun onReadyView() {
+        viewPager = binding.viewPager
+        nextButton = binding.nextButton
+
+        val adapter = OnboardingPagerAdapter(requireActivity())
+        viewPager.adapter = adapter
+
+        nextButton.setOnClickListener {
+            val nextItem = viewPager.currentItem + 1
+            if (nextItem < adapter.itemCount) {
+                viewPager.currentItem = nextItem
+            } else {
+                // Handle the end of the onboarding
+            }
+        }
     }
 }
