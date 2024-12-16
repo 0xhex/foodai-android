08792cbfa1fc36bc75f5593ef6c619c591743d17
mehmettas <m_tas@outlook.com>
2024-12-17 00:20:35 +0300

    Handle navigation issue

--- app/src/main/java/com/codepad/foodai/ui/core/BaseFragment.kt
+++ app/src/main/java/com/codepad/foodai/ui/core/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 abstract class BaseFragment<TBinding : ViewDataBinding> : Fragment() {
 
@@ -33,6 +35,9 @@ abstract class BaseFragment<TBinding : ViewDataBinding> : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         onReadyView()
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().popBackStack()
+        }
     }
 
     open fun navigate(activity: FragmentActivity, view: Int, action: Int) {
