d04033eeb57037e996f9b9be9c5535ba2c0f22ae
mehmettas <m_tas@outlook.com>
2024-12-21 22:48:33 +0300

    Implement recipes page UI & actions

--- app/src/main/java/com/codepad/foodai/ui/home/home/pager/recipe/FoodRecipesFragment.kt
+++ app/src/main/java/com/codepad/foodai/ui/home/home/pager/recipe/FoodRecipesFragment.kt
@@ -1,6 +1,9 @@
 package com.codepad.foodai.ui.home.home.pager.recipe
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codepad.foodai.R
 import com.codepad.foodai.databinding.FragmentFoodRecipesBinding
 import com.codepad.foodai.ui.core.BaseFragment
@@ -14,6 +17,13 @@ class FoodRecipesFragment : BaseFragment<FragmentFoodRecipesBinding>() {
     override fun getLayoutId(): Int = R.layout.fragment_food_recipes
 
     override fun onReadyView() {
-        // Initialize your view here
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val mealTypes = listOf("Breakfast", "Lunch", "Dinner")
+        val adapter = RecipeCardAdapter(mealTypes)
+        binding.rvRecipes.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvRecipes.adapter = adapter
     }
 }
