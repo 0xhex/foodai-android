275e59be5a8ccf1874953a9f9215b412fe9405c7
mehmettas <m_tas@outlook.com>
2024-12-24 23:28:32 +0300

    Set daily health score

--- app/src/main/java/com/codepad/foodai/ui/home/home/pager/health/GoogleHealthFragment.kt
+++ app/src/main/java/com/codepad/foodai/ui/home/home/pager/health/GoogleHealthFragment.kt
@@ -1,20 +1,27 @@
 package com.codepad.foodai.ui.home.home.pager.health
 
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.codepad.foodai.R
 import com.codepad.foodai.databinding.FragmentGoogleHealthBinding
 import com.codepad.foodai.ui.core.BaseFragment
 import com.codepad.foodai.ui.home.HomeViewModel
+import com.codepad.foodai.ui.home.home.pager.HomePagerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GoogleHealthFragment : BaseFragment<FragmentGoogleHealthBinding>() {
-    private val viewModel: HomeViewModel by viewModels()
+    private val sharedViewModel: HomePagerViewModel by activityViewModels()
 
     override fun getLayoutId(): Int = R.layout.fragment_google_health
 
     override fun onReadyView() {
-        // Initialize your view here
+        sharedViewModel.dailySummary.observe(viewLifecycleOwner) {
+            binding.apply {
+                progressBar.progress = (it.healthScore?.times(10))?.toInt() ?: 0
+                txtProgress.text = "${it.healthScore}/10"
+            }
+        }
     }
 
     override fun onResume() {
